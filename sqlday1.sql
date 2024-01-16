4e067f726ff06c84306f5f45c709dd002a5d82c4
venkatesh j m <125080129+venkisamarth@users.noreply.github.com>
2024-01-16 20:08:50 +0530

    Add files via upload

---
+++ sqlday1.sql
@@ -0,0 +1,53 @@
+--session id 
+-- simultaneously  opern 32764 connection in sql 
+-- session id starting with 1-49  are reserved for  sql server internal system system processes
+-- sql sever internal system processes 
+sp_who2
+--sa system  administration 
+--sql exstention 
+-- Session ID
+-- Simultaneusly open 32767 connection in sql 
+--Session id starting with 1-49 are reserverd for the sql severe internal system  process
+sp_who2
+sa-system administration 
+sql extention 
+-- sql is programming language or tool ?
+-- structurd querry language (table (rows and columns))
+-- is there a limit how much data we can store in oure  sql server 
+-- if we have a drive of TB  the elemenate  is TB
+--  I have  worked on a datawarhouse of 74 pb 
+-- BLOB formate 
+-- phone --memory of 128 GB --every picture 
+--- it click  s have of 1 gb 
+--128 -129 
+--defalt database
+-- master  database  record  all the system  level  in formate 
+--msdb - database it used by sql server agaent for scheduling alert and jobs 
+-- model - is used as the template for all the data base  created on the instace of  sql 
+--sql holding temporary
+--temdb- is used to workd space for holiding temparaya objects or intermediate result sets-
+-- How we can create a user database 
+-- Tow ways to create user database 
+-- Request by ur client |TL |project to create a database named as a Demo 
+create database  Demo
+--create database isto create databse named as sqlbatch  
+create database SQLBATCH
+create database balgeet 
+--sql is not the case sensitive language 
+create database [batch sql1]
+create database[sql-batch]
+--creating the database using the GUI
+--right click on database- new database -enter name-ok
+--where ot os storing this database
+
+--where it is storing this data base 
+--right click db-properties -files- copy path -past in file manager
+
+
+
+
+
+
+
+
+
